2541308556901341259c2582b312c3dfbf56fff6
Renan Rodrigo <renanrb@ime.usp.br>
2018-03-06 14:19:56 -0300

    Adapt the NApp to the new Topology format
    Fix #17

--- graph.py
+++ graph.py
@@ -14,14 +14,12 @@ class KytosGraph:
     def update_topology(self, topology):
         """Update all nodes and links inside the graph."""
         self.graph.clear()
-        self.update_nodes(topology.devices)
+        self.update_nodes(topology.switches)
         self.update_links(topology.links)
-        if hasattr(topology, 'circuits'):
-            self.set_links_properties(topology.circuits)
 
     def update_nodes(self, nodes):
         """Update all nodes inside the graph."""
-        for node in nodes:
+        for node in nodes.values():
             try:
                 self.graph.add_node(node.id)
 
@@ -34,36 +32,25 @@ class KytosGraph:
 
     def update_links(self, links):
         """Update all links inside the graph."""
-        for source, destination in links:
-            self.graph.add_edge(source, destination)
+        keys = []
+        for link in links.values():
+            self.graph.add_edge(link.endpoint_a.id, link.endpoint_b.id)
+            for key, value in link.metadata.items():
+                keys.extend(key)
+                self.graph[link.endpoint_a.id][link.endpoint_b.id][key] = value
 
-    def set_links_properties(self, circuits):
-        """Set properties for links in the graph."""
-        for circuit in circuits:
-            self._remove_switch_hops(circuit)
-            if len(circuit['hops']) == 2:
-                self.set_link_properties(circuit)
-        if circuits:
-            self._set_switch_links_properties(circuits[0]['custom_properties'])
+        self._set_default_metadata(keys)
 
-    def set_link_properties(self, circuit):
-        """Set properties for a single link in the graph."""
-        hop_a = circuit['hops'][0]
-        hop_b = circuit['hops'][1]
-        if [hop_a, hop_b] in self.graph.edges:
-            for prop, value in circuit['custom_properties'].items():
-                self.graph[hop_a][hop_b][prop] = value
+    def _set_default_metadata(self, keys):
+        """Set metadata to all links.
 
-    def _set_switch_links_properties(self, properties):
-        """Set properties to interface-to-switch link abstractions.
-
-        The value is zero to make those irrelevant in pathfinding.
+        Set the value to zero for inexistent metadata in a link to make those
+        irrelevant in pathfinding.
         """
-        for prop in properties:
-            for a,b in self.graph.edges:
-                if prop not in self.graph[a][b]:
-                    self.graph[a][b][prop] = 0
-
+        for key in keys:
+            for endpoint_a, endpoint_b in self.graph.edges:
+                if key not in self.graph[endpoint_a][endpoint_b]:
+                    self.graph[endpoint_a][endpoint_b][key] = 0
 
     @staticmethod
     def _remove_switch_hops(circuit):
