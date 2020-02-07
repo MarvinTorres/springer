bedd33a5355d33b876c180c542c329843adb642e
Daniela Garcia Toranzo <43040493+dgarc330@users.noreply.github.com>
2020-02-07 17:01:34 -0500

    Update __init__.py
    Added updated code to fix a bug in the previous __init__ file.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,10 @@
 """Module to test kytos/pathfinder."""
+import sys
+import os
+from pathlib import Path
+​
+BASE_ENV = Path(os.environ.get('VIRTUAL_ENV', '/'))
+​
+NAPPS_DIR = BASE_ENV / 'var/lib/kytos/'
+​
+sys.path.insert(0, str(NAPPS_DIR))
